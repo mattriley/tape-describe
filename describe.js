c3b4711c3e33c35c185826f3c4ef898763bc0d61
Matt Riley <mattrileyaus@gmail.com>
2020-07-14 08:56:25 +1000

    assign existing properties of tape to decorator

--- describe.js
+++ describe.js
@@ -1,6 +1,6 @@
 module.exports = tape => (subject, callback) => {
     const testWith = func => (name, ...args) => func(`${subject}: ${name}`, ...args);
-    const test = Object.assign(testWith(tape), {
+    const test = Object.assign(testWith(tape), tape, {
         only: testWith(tape.only),
         skip: testWith(tape.skip)
     });
