0828fb2f98800df05a1b394de2f3b6d7ea95e473
Matt Riley <mattrileyaus@gmail.com>
2020-07-17 12:07:53 +1000

    update tape-decorate

--- describe.js
+++ describe.js
@@ -5,7 +5,7 @@ const def = {
 
 module.exports = ({ decorate = def.decorate, separator = def.separator }) => tape => (subject, cb) => {
     const decorator = test => (name, ...args) => test([subject, name].join(separator), ...args)
-    const test = decorate(tape, decorator);
+    const test = decorate(tape)(decorator);
     if (cb) cb(test);
     return test;
 };
